912c7ff7f55af6182bd7cee70beba9eec7e4a9da
Paul von Hagen <pvonhagen26@gmail.com>
2020-04-17 00:08:32 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,22 @@
+require_relative "../config/environment.rb"
+
+class Dog
+attr_accessor :name, :breed
+attr_reader :idea
+
+  def initialize(id: = nil, name:, breed:)
+    @id = idea
+    @name = name
+    @breed = breed
+  end
+
+  def self.create_table
+    sql = <<-SQL
+      create table if not exists dogs (
+        id integer primary key,
+        name text,
+        breed text
+      )
+      SQL
+    DB[:conn].execute(sql)
+  end
