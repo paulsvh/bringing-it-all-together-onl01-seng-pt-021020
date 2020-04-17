24687718c34d87290a6539dedf1ec7308906638f
Paul von Hagen <pvonhagen26@gmail.com>
2020-04-17 00:22:41 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -45,7 +45,13 @@ attr_reader :id
     new_dog
   end
 
-
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    breed = row[2]
+    new_dog = self.new(id, name, breed)
+    new_dog
+  end
 
 
 
@@ -53,3 +59,4 @@ attr_reader :id
 
 
 end
+
